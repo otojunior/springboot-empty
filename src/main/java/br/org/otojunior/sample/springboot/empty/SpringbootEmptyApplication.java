ce2c60880fdb133abf190b969a0b48d6a0e086b7
Oto Soares Coelho Junior <oto.coelho-junior@serpro.gov.br>
2018-07-26 16:23:13 -0300

    Commit inicial

---
+++ src/main/java/br/org/otojunior/sample/springboot/empty/SpringbootEmptyApplication.java
@@ -0,0 +1,23 @@
+/*
+ * 
+ */
+package br.org.otojunior.sample.springboot.empty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ * @author Oto Soares Coelho Junior (oto.coelho-junior@serpro.gov.br)
+ *
+ */
+@SpringBootApplication
+public class SpringbootEmptyApplication {
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootEmptyApplication.class, args);
+	}
+}
