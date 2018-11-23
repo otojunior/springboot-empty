0da39c8d7c8dac9d65e6ede113aef2b61753dbcf
Oto Soares Coelho Junior <oto.coelho-junior@serpro.gov.br>
2018-11-23 10:58:27 -0200

    Troca da implementação de banco para H2

--- src/test/java/br/org/otojunior/sample/springboot/empty/repository/ItemRepositoryTest.java
+++ src/test/java/br/org/otojunior/sample/springboot/empty/repository/ItemRepositoryTest.java
@@ -8,10 +8,13 @@ import static org.junit.Assert.assertNull;
 
 import java.util.List;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import br.org.otojunior.sample.springboot.empty.entity.Item;
@@ -25,15 +28,31 @@ import br.org.otojunior.sample.springboot.empty.entity.Item;
 public class ItemRepositoryTest {
 	@Autowired
 	private ItemRepository repository;
+	
+	@Autowired
+	private JdbcTemplate template;
+	
+	@Before
+	public void beforeClass() {
+		template.update("create table item (\n" + 
+			"  id bigserial primary key,\n" + 
+			"  codigo integer,\n" + 
+			"  nome varchar(20))");
+	}
+	
+	@After
+	public void afterClass() {
+		template.update("drop table item");
+	}
 
 	/**
 	 * Test method for {@link br.org.otojunior.sample.springboot.empty.repository.ItemRepository#findByCodigo(java.lang.Integer)}.
 	 */
 	@Test
 	public void testFindByCodigo() {
+		repository.insertItem(100, "item10");
 		List<Item> itens = repository.findByCodigo(100);
 		Item item = itens.get(0);
-		assertEquals(10L, item.getId().longValue());
 		assertEquals(100, item.getCodigo().intValue());
 		assertEquals("item10", item.getNome());
 	}
@@ -43,6 +62,7 @@ public class ItemRepositoryTest {
 	 */
 	@Test
 	public void testFindNomeByCodigo() {
+		repository.insertItem(100, "item10");
 		List<Item> itens = repository.findNomeByCodigo(100);
 		Item item = itens.get(0);
 		assertNull(item.getId());
